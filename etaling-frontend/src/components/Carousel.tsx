96275a6c01aa8c0920a85cde38ad2c58c285fb07
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-02-19 23:40:15 +0530

    Create a dummy Carousel component
    Also installs csstype as a dev dependency
    
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

---
+++ etaling-frontend/src/components/Carousel.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react';
+
+import { Box } from '@mui/material';
+
+import { Property } from 'csstype';
+
+export default function Carousel<T>({
+  items,
+  itemToTile,
+  style = defaultStyle,
+}: CarouselProps<T>) {
+  return (
+    <Box
+      display={'flex'}
+      sx={{
+        gap: style.gap,
+      }}
+    >
+      {items.map((item, idx) => itemToTile(item, idx))}
+    </Box>
+  );
+}
+
+const defaultStyle: CarouselStyleProps = {
+  gap: 0,
+};
+
+export interface CarouselProps<T> {
+  items: T[];
+  itemToTile: (item: T, idx: number) => ReactNode;
+
+  style: CarouselStyleProps;
+}
+
+export interface CarouselStyleProps {
+  gap: Property.Gap;
+}
