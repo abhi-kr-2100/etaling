dd89171d2e269da093d72abc2dc96c2bac6b2980
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-02-19 23:10:29 +0530

    Add theming
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

---
+++ etaling-frontend/src/assets/theme.tsx
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: { main: '#58cc02' },
+    secondary: { main: '#FFFFFF' },
+  },
+});
+
+export default theme;
--- etaling-frontend/src/main.tsx
+++ etaling-frontend/src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { ThemeProvider } from '@emotion/react';
+
 import './i18next.ts';
+
+import theme from './assets/theme.tsx';
 
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
