676d0dd6fd17595d7b1fba8a322c9dfc2898567e
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-03-02 02:07:16 +0530

    Remove horizontal scrollbar
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- etaling-frontend/src/Homepage/index.tsx
+++ etaling-frontend/src/Homepage/index.tsx
@@ -12,8 +12,6 @@ export default function Homepage() {
       flexDirection={'column'}
       justifyContent={'center'}
       alignItems={'center'}
-      width={'100%'}
-      height={'100%'}
     >
       <EtalingHeader />
       <SupportedLanguagesCarousel supportedLanguages={supportedLanguages} />
