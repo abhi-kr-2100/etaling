cef8053114035702502c2de68cbdaab9a047351f
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-03-03 00:08:40 +0530

    Setup eslint and prettier
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

---
+++ etaling-express-backend/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
