6af606a56ac268d507a0d0dea3cbcb2706281f0e
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-03-08 01:57:45 +0530

    Use createUserProfile middleware
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- etaling-express-backend/src/server.ts
+++ etaling-express-backend/src/server.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
+import './db';
 
 import express from 'express';
 
 import cors from 'cors';
 import jwtCheck from './middlewares/jwt-check';
+import createUserProfile from './middlewares/createUserProfile';
 
 const PORT = process.env.PORT ?? 3000;
 
@@ -11,6 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(jwtCheck);
+app.use(createUserProfile);
 
 app.get('/api', (req, res) => {
   console.log(req);
