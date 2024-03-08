c82c582c98057bb8e08d7ef754e06d063e56e63a
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-03-08 12:47:04 +0530

    Add some basic language model interfaces
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

---
+++ express-backend/src/language-models/index.ts
@@ -0,0 +1,22 @@
+import { LanguageCode } from '../../../webapp/src/assets/language';
+
+export interface LanguageModel {
+  getLanguage(): LanguageCode;
+  getWords(sentence: string): string[];
+}
+
+/**
+ * An LM where a sentence can be split into words simply by spliting them
+ * across word separators.
+ *
+ * For example, in English, words are separated by whitespace and some
+ * punctuations.
+ */
+export interface WordSeparatedLM extends LanguageModel {
+  getSeparatorRegex(): RegExp;
+}
+
+export interface CaseSensitiveLM extends LanguageModel {
+  toLowerCase(s: string): string;
+  toUpperCase(s: string): string;
+}
