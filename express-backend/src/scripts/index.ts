2b56eda8a3d86b4ad8571551793aa9fd9880855d
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-03-17 00:16:27 +0530

    Add file to run internal scripts
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

---
+++ express-backend/src/scripts/index.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import '../db';
+
+import TatoebaSentenceListCreator from './SentenceListCreator/TatoebaSentenceListCreator';
+
+import { UserProfile } from '../user-profile';
+import { LanguageCode } from '../../../shared/languages';
+
+if (process.argv.length < 3) {
+  process.exit();
+}
+
+switch (process.argv[2]) {
+  case 'tatoeba': {
+    if (process.argv.length !== 6) {
+      console.error('Missing required params: title, user._id, language');
+      process.exit(1);
+    }
+
+    const user = await UserProfile.findById(process.argv[4]);
+    const sentenceListCreator = new TatoebaSentenceListCreator(
+      process.argv[3],
+      user,
+      true,
+      process.argv[5] as LanguageCode,
+    );
+
+    await sentenceListCreator.execute();
+    break;
+  }
+  default:
+    console.error('Unknown operation');
+}
