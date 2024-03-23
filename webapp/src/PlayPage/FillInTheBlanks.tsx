4c3c6d4264aa2d517f462d19772b69c69ae57a12
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-03-23 13:37:07 +0530

    Allow users to go to the next question
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- webapp/src/PlayPage/FillInTheBlanks.tsx
+++ webapp/src/PlayPage/FillInTheBlanks.tsx
@@ -5,16 +5,23 @@ import { SentenceData } from './Play';
 import { getLanguageModel } from '../../../express-backend/src/language-models';
 import { WordScoreType } from '../../../express-backend/src/word/wordScore';
 import { Typography } from '@mui/material';
+import { useMemo } from 'react';
 
 export default function FillInTheBlanks({
   sentenceData,
 }: FillInTheBlanksProps) {
   const text = sentenceData.sentence.text!;
-  const wordToMask = sample(sentenceData.words) as WordScoreType;
+  const wordToMask = useMemo(
+    () => sample(sentenceData.words) as WordScoreType,
+    [sentenceData],
+  );
 
   const lm = getLanguageModel(sentenceData.sentence.textLanguageCode!);
   const occurrences = lm.findWord(text, wordToMask.word!.wordText!);
-  const occurrenceToMask = sample(occurrences) as [number, number];
+  const occurrenceToMask = useMemo(
+    () => sample(occurrences) as [number, number],
+    [occurrences],
+  );
 
   const maskedText =
     text.slice(0, occurrenceToMask[0]) +
--- webapp/src/PlayPage/Play.tsx
+++ webapp/src/PlayPage/Play.tsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import { SentenceType } from '../../../express-backend/src/sentence';
 import { WordScoreType } from '../../../express-backend/src/word/wordScore';
 import FillInTheBlanks from './FillInTheBlanks';
+import { Box, Button } from '@mui/material';
 
 export default function Play({ sentences }: PlayProps) {
+  const [currQuestionIdx, setCurrQuestionIdx] = useState(0);
+
   return (
-    <>
-      {sentences.map((s) => (
-        <FillInTheBlanks sentenceData={s} key={s.sentence.text} />
-      ))}
-    </>
+    <Box
+      display={'flex'}
+      flexDirection={'column'}
+      sx={{
+        gap: 1.5,
+      }}
+      alignItems={'center'}
+    >
+      <FillInTheBlanks sentenceData={sentences[currQuestionIdx]} />
+      <Button
+        disabled={currQuestionIdx == sentences.length - 1}
+        onClick={() => setCurrQuestionIdx((prev) => prev + 1)}
+      >
+        Next
+      </Button>
+    </Box>
   );
 }
 
