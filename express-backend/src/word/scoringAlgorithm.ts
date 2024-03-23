3f7e7f61d0464fea64659a90830ec5f28e90f5fe
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-03-23 21:11:03 +0530

    Clarify parameter name
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- express-backend/src/word/scoringAlgorithm.ts
+++ express-backend/src/word/scoringAlgorithm.ts
@@ -5,29 +5,29 @@ export type GradeType = 0 | 1 | 2 | 3 | 4 | 5;
 // Reference: https://en.wikipedia.org/wiki/SuperMemo#Description_of_SM-2_algorithm
 export default function getUpdatedWordScore(
   grade: GradeType,
-  score: ScoreType,
+  oldScore: ScoreType,
 ): ScoreType {
-  let updatedScore = score;
+  let updatedScore = oldScore;
 
   if (grade >= 3) {
-    if (score.repetitionNumber === 0) {
+    if (oldScore.repetitionNumber === 0) {
       updatedScore.interRepetitionIntervalInDays = 1;
-    } else if (score.repetitionNumber === 1) {
+    } else if (oldScore.repetitionNumber === 1) {
       updatedScore.interRepetitionIntervalInDays = 6;
     } else {
       updatedScore.interRepetitionIntervalInDays = Math.round(
-        score.interRepetitionIntervalInDays * score.easinessFactor,
+        oldScore.interRepetitionIntervalInDays * oldScore.easinessFactor,
       );
     }
 
-    updatedScore.repetitionNumber = score.repetitionNumber + 1;
+    updatedScore.repetitionNumber = oldScore.repetitionNumber + 1;
   } else {
     updatedScore.repetitionNumber = 0;
     updatedScore.interRepetitionIntervalInDays = 1;
   }
 
   const easinessFactor =
-    score.easinessFactor + (0.1 - (5 - grade) * (0.08 + (5 - grade) * 0.02));
+    oldScore.easinessFactor + (0.1 - (5 - grade) * (0.08 + (5 - grade) * 0.02));
   updatedScore.easinessFactor = Math.max(1.3, easinessFactor);
 
   updatedScore.lastReviewDate = new Date();
