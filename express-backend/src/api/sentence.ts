2bf9c26396c47918e96f0751edbbd60c8ae88ed7
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-03-24 02:10:49 +0530

    Fix control flow of updateScore
    Call next before returning.
    
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- express-backend/src/api/sentence.ts
+++ express-backend/src/api/sentence.ts
@@ -1,22 +1,24 @@
-import { Request, Response, Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import SentenceScore from '../sentence/sentenceScore';
 import getUpdatedWordScore, { GradeType } from '../word/scoringAlgorithm';
 
-export async function updateScore(req: Request, res: Response) {
+export async function updateScore(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   const grade = Number.parseInt(req.query.grade as string);
 
   if (grade < 0 || grade > 5) {
-    return res.status(400).json({
-      error: 'Score must be between 0 and 5 inclusive.',
-    });
+    res.status(400);
+    return next('Score must be between 0 and 5 inclusive.');
   }
 
   const sentenceScoreId = req.params.id;
   const sentenceScore = await SentenceScore.findById(sentenceScoreId);
   if (sentenceScore === null) {
-    return res.status(404).json({
-      error: 'Sentence score was not found.',
-    });
+    res.status(404);
+    return next('Sentence score was not found.');
   }
 
   const updatedScore = getUpdatedWordScore(
@@ -26,10 +28,10 @@ export async function updateScore(req: Request, res: Response) {
   sentenceScore.score = updatedScore;
   await sentenceScore.save();
 
-  return res.status(200);
+  res.status(200).json({ message: 'Score updated.' });
 }
 
 const router = Router();
-router.post('/:id/updateScore');
+router.post('/:id/updateScore', updateScore);
 
 export default router;
