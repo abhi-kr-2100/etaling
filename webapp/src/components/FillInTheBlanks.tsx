7f6710442306da49d2e8fae1c9b4549ebefe20b0
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-03-23 19:11:59 +0530

    Allow showing an optional hint
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- webapp/src/components/FillInTheBlanks.tsx
+++ webapp/src/components/FillInTheBlanks.tsx
@@ -3,19 +3,30 @@ import { Box, TextField, TextFieldProps, Typography } from '@mui/material';
 export default function FillInTheBlanks({
   textBeforeBlank,
   textAfterBlank,
+  hint,
   BlankInputProps,
 }: FillInTheBlanksProps) {
   return (
     <Box
       display={'flex'}
-      alignItems={'center'}
+      flexDirection={'column'}
       sx={{
-        gap: 1.5,
+        gap: 1,
       }}
+      alignItems={'center'}
     >
-      <Typography>{textBeforeBlank}</Typography>
-      <TextField {...BlankInputProps} />
-      <Typography>{textAfterBlank}</Typography>
+      <Box
+        display={'flex'}
+        alignItems={'center'}
+        sx={{
+          gap: 1.5,
+        }}
+      >
+        <Typography>{textBeforeBlank}</Typography>
+        <TextField {...BlankInputProps} />
+        <Typography>{textAfterBlank}</Typography>
+      </Box>
+      <Typography>{hint}</Typography>
     </Box>
   );
 }
@@ -23,5 +34,6 @@ export default function FillInTheBlanks({
 export interface FillInTheBlanksProps {
   textBeforeBlank: string;
   textAfterBlank: string;
+  hint?: string;
   BlankInputProps: TextFieldProps;
 }
