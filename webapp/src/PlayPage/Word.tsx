98334bc0684d2e7cb11e82b75de754b7acfa6d58
Abhishek Kumar <abhi.kr.2100@gmail.com>
2024-04-07 14:20:56 +0530

    Indicate easiness factor of words via color
    Signed-off-by: Abhishek Kumar <abhi.kr.2100@gmail.com>

--- webapp/src/PlayPage/Word.tsx
+++ webapp/src/PlayPage/Word.tsx
@@ -2,9 +2,12 @@ import { Chip } from '@mui/material';
 import { CorrectedWordScoreType } from './Play';
 
 export default function Word({ wordText, wordScore }: WordProps) {
+  const ef = wordScore.score.easinessFactor;
+  const color = ef < 2.5 ? 'error' : ef === 2.5 ? 'info' : 'success';
+
   return (
     <Chip
-      color="primary"
+      color={color}
       size="small"
       label={wordText || wordScore.word!.wordText!}
     />
